3fd609c160745542e4c7c2edbe1250a3c898e131
Piotr <bartnic1@gmail.com>
2018-03-03 04:34:00 +0000

    Added better comments to app.js file

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,16 +4,15 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-//Created this function to find time difference between now and previous tweets
-//Only goes down to the hour, but can be easily modified to go to minutes and seconds.
+//This function converts differences in epoch seconds to differences in years, months, days, and hours.
+//Note: This uses average seconds in a year, month etc. While the time elapsed is not perfectly precise,
+//it is more than sufficient for this application.
 function findTimeDifference(today, tweet){
   let diffSeconds = Math.floor(today/1000) - Math.floor(tweet/1000);
   let returnArray = [];
-  //Function converts epoch seconds to years, months, days, hours, minutes, seconds.
-  //Note: This uses average seconds in a year, month etc. While the time elapsed is not perfectly precise,
-  //it is more than sufficient for this application.
+
   function getTimeElapsed(epochSeconds){
-    // time = [[years,yearSeconds], [months,monthSeconds], [days, daySeconds], [hours, hourSeconds]]
+    // Time = [[years,yearSeconds], [months,monthSeconds], [days, daySeconds], [hours, hourSeconds]]
     let time = [[0,31556926], [0,2629743], [0,86400], [0,3600]];
     for(let timeIndex in time){
       time[timeIndex][0] = Math.floor(epochSeconds/time[timeIndex][1]);
@@ -70,9 +69,9 @@ function findTimeDifference(today, tweet){
   let returnString = returnArray.join(", ") + ' ago';
   return returnString;
 }
-//This generates the html for a n)ew tweet
+
+//This generates the html for a new tweet and returns it.
 function createTweetElement(tweetData){
-  $( "#greatphoto" ).attr( "alt", "Beijing Brush Seller" );
   let $tweet = $("<div>").addClass("tweet");
   let today = new Date().getTime();
   let timeDifference = findTimeDifference(today, tweetData.created_at);
@@ -118,6 +117,8 @@ function loadTweets(){
   });
 }
 
+//These functions ensure that only logged-in users have access to certain UI options, such as composing
+//new messages.
 function showOptions() {
   $(".compose").css("visibility", "visible");
   $(".logout-span").css("visibility", "visible");
@@ -133,12 +134,14 @@ function hideOptions() {
   $(".welcome").text('');
 }
 
+//Add user function for the registration form, for drier code
 function addUser(userFormData){
   $.post("/users/", userFormData).done(function(res){
     showOptions();
   })
 }
 
+//Clears form entries for future use
 function clearEntries(name, pass){
   name.val('');
   pass.val('');
@@ -151,8 +154,7 @@ let allowLikes = false;
 $(document).ready(function(){
   loadTweets();
 
-//When the a new tweet is created, assuming it passes relevant conditions, the serialized data
-//is sent to a server, after which it is immediately loaded on the screen using loadTweets().
+//This event handler is used to generate tweets. For logged-in users, the cookie session ID is used to generate a userID and handle. Creation of a unique avatar is a work in progress.
   $("#tweetForm").on('submit', function(event){
     event.preventDefault();
     let errorMessage = $(".new-tweet").find(".error-message")
@@ -175,13 +177,13 @@ $(document).ready(function(){
     });
   });
 
-  //This jQuery script allows the new tweet form to slide up and down upon clicking the compose button.
+  //This jQuery script allows the "new tweet" form to slide up and down upon clicking the compose button.
   $(".compose").on('click', function(event){
     $(".new-tweet").slideToggle("fast");
     $(".new-tweet").find("textarea").focus();
   });
 
-  //If database has no users, then add a user. Else, check if username taken. If not, add user.
+  //The registration event first checks if database has no users; it so, then it adds a new user and auto-logs them in. Otherwise, it checks if the username has been taken before creating a new user..
   $("#registerForm").on('submit', function(event){
     event.preventDefault();
 
@@ -212,8 +214,7 @@ $(document).ready(function(){
     })
   })
 
-  //Check if username and password is correct. If so, clear entries for later use and make put request
-  //to create user cookie.
+  //The login form first checks if the username and password are correct. If so, it clears entries for later use and makes a put request to generate (an encrypted) user cookie.
   $("#loginForm").on('submit', function(event){
     event.preventDefault();
     let userFormData = $(this).serialize();
@@ -242,6 +243,8 @@ $(document).ready(function(){
     });
   });
 
+  //Logs the user out, and deletes the current session cookie. Also prevents the now anonymous
+  //user from liking or adding any new tweets
   $(".logout-button").on('click', function(event){
     $.post("/users/logout?_method=PUT").done(function(res){
       hideOptions();
@@ -249,18 +252,21 @@ $(document).ready(function(){
     })
   });
 
-
+  //This event listender checks if a tweet was liked
   $("#tweets-container").on('click', function(event){
     if(allowLikes){
       let target = event.target;
       if(target.classList[1] === "like"){
+
         //Immediately increment number of likes
         let likesCounter = $(target).siblings(".likes");
         likesCounter.text(+likesCounter.text() + 1);
+
         //Get user handle to test if liking user's own tweet, or other tweet more than once
         let userHandle = $(target).data('handle');
         console.log('handle', userHandle);
         console.log('currentlikes', likesCounter.text());
+
         //Now update database for persistency
         userObj = {handle: userHandle, likes: likesCounter.text()}
         $.post("/tweets/?_method=PUT", userObj).done(function(res){
@@ -268,8 +274,7 @@ $(document).ready(function(){
             likesCounter.text(+likesCounter.text() - 1);
           }
         });
-    }
-
+      }
     }
   });
 });
